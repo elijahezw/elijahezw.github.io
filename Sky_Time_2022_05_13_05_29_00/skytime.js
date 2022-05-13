e0944dbf79c5b196df2fc31246b97352956ed471
elijahezw <101950063+elijahezw@users.noreply.github.com>
2022-05-12 22:38:17 -0700

    Add files via upload

---
+++ Sky_Time_2022_05_13_05_29_00/skytime.js
@@ -0,0 +1,50 @@
+let canvas = '#ffffff';
+let arcs = 48
+let i = 0
+let col;
+
+function preload() {
+    col = loadStrings('skycolor.txt');
+}
+
+function setup() {
+  let canvas = createCanvas(640, 640);
+  frameRate(1);
+  canvas.parent('sky-time');
+  
+}
+
+function draw() {  
+  background(canvas);
+  
+ if (i < col.length) {
+      let subwidth = (TWO_PI/arcs);
+        
+  for (var j = 0; j <= i; j++) {
+      let st = col[j];
+      print(st);
+      noStroke();
+      fill(st);
+      arc (320, 320, 400, 400, subwidth * j, 
+           subwidth * j + subwidth); 
+  
+      let bright = brightness(st);
+      fill(canvas);
+      noStroke();
+      arc (320, 320,  bright *1.5, bright *1.5, 
+           subwidth * j -0.01, subwidth * j + subwidth+0.01); 
+      
+      fill(canvas);
+      noStroke();
+      circle (320, 320, 100);
+  }  
+        
+  print(i);
+      i ++;
+      
+      if (i>arcs-1){
+        i = 0;}
+
+  }
+}
+  
